1376bb9cff4a98748209bb15b272ff9f2bab38b0
kaori kohama <kkaori131415@gmail.com>
2020-05-19 12:14:06 +0200

    styled calender

--- app/javascript/components/home/calender.js
+++ app/javascript/components/home/calender.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react"
-import { Inject, ScheduleComponent, Day, Week, Month } from '@syncfusion/ej2-react-schedule';
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule';
 
 class Calender extends Component {
   render(){
     return <ScheduleComponent>
-      <Inject services={[Day, Week, Month ]} />
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda ]} />
     </ScheduleComponent>
   }
 }
