cb2c7b9373f591b1626230e882fce0ec8ccda5f0
kaori kohama <kkaori131415@gmail.com>
2020-05-25 18:09:03 +0200

    added relation between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :goals
+  has_many :journals
+  has_many :projects
+  has_many :to_dos
 end
